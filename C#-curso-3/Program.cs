6bf3ab2a1c459d569d2ed5c6c79d3e2e18929029
luciano-repetti <lucianorepett2020@gmail.com>
2026-03-17 18:15:35 -0300

    Add Program entry point with component selection via enum

---
+++ C#-curso-3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Seleccione un componente con numero 1 - 4");
+var componentesDisponibles = Enum.GetValues(typeof(TipoComponente));
+// var seleccion
+
+// for (int i = 0; i < length; i++)
+// {
+    
+// }
